8084d284293e6008829f085ec8015a402e97f768
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-12-27 15:15:54 +0800

    基本的排序算法一览

--- src/SortBase.java
+++ src/SortBase.java
@@ -1,3 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ 基本的排序算法一览
+ * */
 public class SortBase {
 
     /**
@@ -102,6 +109,285 @@ public class SortBase {
     }
 
 
+    /**
+     自底向上归并排序(循环控制)
+
+     先归并那些微型数组，然后成对归并得到的微型数组。
+     * */
+    static class Down2UpMergeSort<T extends Comparable<T>> extends MergeSort<T> {
+        @Override
+        public void sort(T[] nums) {
+
+            int N = nums.length;
+            aux = (T[]) new Comparable[N];
+
+            for (int sz = 1; sz < N; sz += sz) {
+                for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                    merge(nums, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+                }
+            }
+        }
+    }
+
+
+
+    /**
+     自顶向下归并排序（递归）
+
+     将一个大数组分成两个小数组去求解。
+     因为每次都将问题对半分成两个子问题，这种对半分的算法复杂度一般为 O(NlogN)。
+     * */
+    static class Up2DownMergeSort<T extends Comparable<T>> extends MergeSort<T> {
+        @Override
+        public void sort(T[] nums) {
+            aux = (T[]) new Comparable[nums.length];
+            sort(nums, 0, nums.length - 1);
+        }
+
+        private void sort(T[] nums, int l, int h) {
+            if (h <= l) {
+                return;
+            }
+            int mid = l + (h - l) / 2;
+            sort(nums, l, mid);
+            sort(nums, mid + 1, h);
+            merge(nums, l, mid, h);
+        }
+    }
+
+
+
+
+
+    /**
+     二向切分分快速排序
+
+     快速排序是原地排序，不需要辅助数组，但是递归调用需要辅助栈。
+     快速排序最好的情况下是每次都正好能将数组对半分，这样递归调用次数才是最少的。这种情况下比较次数为 CN=2CN/2+N，复杂度为 O(NlogN)。
+     最坏的情况下，第一次从最小的元素切分，第二次从第二小的元素切分，如此这般。因此最坏的情况下需要比较 N2/2。为了防止数组最开始就是有序的，在进行快速排序时需要随机打乱数组。
+     * */
+    static class QuickSort<T extends Comparable<T>> extends Sort<T> {
+
+        @Override
+        public void sort(T[] nums) {
+            shuffle(nums);
+            sort(nums, 0, nums.length - 1);
+        }
+
+        private void sort(T[] nums, int l, int h) {
+            if (h <= l) {
+                return;
+            }
+            int j = partition(nums, l, h);
+            sort(nums, l, j - 1);
+            sort(nums, j + 1, h);
+        }
+
+        private void shuffle(T[] nums) {
+            List<Comparable> list = Arrays.asList(nums);
+            Collections.shuffle(list);
+            list.toArray(nums);
+        }
+
+        private int partition(T[] nums, int l, int h) {
+            int i = l, j = h + 1;
+            T v = nums[l];
+            while (true) {
+                while (less(nums[++i], v) && i != h){
+
+                } ;
+                while (less(v, nums[--j]) && j != l){
+
+                } ;
+                if (i >= j){
+                    break;
+                }
+                swap(nums, i, j);
+            }
+            swap(nums, l, j);
+            return j;
+        }
+    }
+
+
+    /**
+     三向切分快速排序
+
+     对于有大量重复元素的数组，可以将数组切分为三部分，分别对应小于、等于和大于切分元素。
+     三向切分快速排序对于只有若干不同主键的随机数组可以在线性时间内完成排序。
+     * */
+    static class ThreeWayQuickSort<T extends Comparable<T>> extends QuickSort<T> {
+
+        protected void sort(T[] nums, int l, int h) {
+            if (h <= l) {
+                return;
+            }
+            int lt = l, i = l + 1, gt = h;
+            T v = nums[l];
+            while (i <= gt) {
+                int cmp = nums[i].compareTo(v);
+                if (cmp < 0) {
+                    swap(nums, lt++, i++);
+                } else if (cmp > 0) {
+                    swap(nums, i, gt--);
+                } else {
+                    i++;
+                }
+            }
+            sort(nums, l, lt - 1);
+            sort(nums, gt + 1, h);
+        }
+    }
+
+    /**
+     堆排序
+     把最大元素和当前堆中数组的最后一个元素交换位置，并且不删除它，那么就可以得到一个从尾到头的递减序列，从正向来看就是一个递增序列，这就是堆排序。
+
+     无序数组建立堆最直接的方法是从左到右遍历数组进行上浮操作。
+     一个更高效的方法是从右至左进行下沉操作，如果一个节点的两个节点都已经是堆有序，那么进行下沉操作可以使得这个节点为根节点的堆有序。
+     叶子节点不需要进行下沉操作，可以忽略叶子节点的元素，因此只需要遍历一半的元素即可。
+     * */
+    static class HeapSort<T extends Comparable<T>> extends Sort<T> {
+        /**
+         * 数组第 0 个位置不能有元素
+         */
+        @Override
+        public void sort(T[] nums) {
+            int N = nums.length - 1;
+            //创建小根堆
+            for (int k = N / 2; k >= 1; k--) {
+                int temp=k;
+                sink(nums, temp, N);
+            }
+
+            //调整小根堆
+            while (N > 1) {
+                swap(nums, 1, N--);
+                sink(nums, 1, N);
+            }
+        }
+
+        private void sink(T[] nums, int k, int N) {
+            while (2 * k <= N) {
+                int j = 2 * k;
+                if (j < N && less(nums, j, j + 1)) {
+                    j++;
+                }
+                if (!less(nums, k, j)) {
+                    break;
+                }
+                swap(nums, k, j);
+                k = j;
+            }
+        }
+
+        private boolean less(T[] nums, int i, int j) {
+            return nums[i].compareTo(nums[j]) < 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr={3,2,4,7,6,5};
+        System.out.println("待排序数组为：");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+
+
+        Integer[] arr1=arr.clone();
+        Selection selection=new SortBase.Selection<>();
+        selection.sort(arr1);
+        System.out.println("选择排序结果： ");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+        }
+        System.out.println();
+
+
+        Integer[] arr2=arr.clone();
+        Bubble bubble=new SortBase.Bubble();
+        bubble.sort(arr2);
+        System.out.println("冒泡排序结果： ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+        }
+        System.out.println();
+
+
+
+        Integer[] arr3=arr.clone();
+        Insertion insertion=new SortBase.Insertion<>();
+        insertion.sort(arr3);
+        System.out.println("插入排序结果： ");
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i]);
+        }
+        System.out.println();
+
+
+        Integer[] arr4=arr.clone();
+        Shell shell=new SortBase.Shell<>();
+        shell.sort(arr4);
+        System.out.println("希尔排序结果： ");
+        for (int i = 0; i < arr4.length; i++) {
+            System.out.print(arr4[i]);
+        }
+        System.out.println();
+
+
+        Integer[] arr5=arr.clone();
+        Down2UpMergeSort dumerge=new SortBase.Down2UpMergeSort<>();
+        dumerge.sort(arr5);
+        System.out.println("自底向上排序结果： ");
+        for (int i = 0; i < arr5.length; i++) {
+            System.out.print(arr5[i]);
+        }
+        System.out.println();
+
+        Integer[] arr6=arr.clone();
+        Up2DownMergeSort udmerge=new SortBase.Up2DownMergeSort<>();
+        udmerge.sort(arr6);
+        System.out.println("自顶向下归并排序结果： ");
+        for (int i = 0; i < arr6.length; i++) {
+            System.out.print(arr6[i]);
+        }
+        System.out.println();
+
+
+        Integer[] arr7=arr.clone();
+        QuickSort quick=new SortBase.QuickSort<>();
+        quick.sort(arr7);
+        System.out.println("二向快速排序结果： ");
+        for (int i = 0; i < arr7.length; i++) {
+            System.out.print(arr7[i]);
+        }
+        System.out.println();
+
+        Integer[] arr8=arr.clone();
+        ThreeWayQuickSort tquick=new SortBase.ThreeWayQuickSort<>();
+        tquick.sort(arr8);
+        System.out.println("三向快速排序结果： ");
+        for (int i = 0; i < arr8.length; i++) {
+            System.out.print(arr8[i]);
+        }
+        System.out.println();
+
+
+
+        Integer[] arr9=new Integer[arr.length+1];
+        for (int i = 1; i <= arr.length; i++) {
+            arr9[i]=arr[i-1];
+        }
+        HeapSort heap=new SortBase.HeapSort<>();
+        heap.sort(arr9);
+        System.out.println("堆排序结果： ");
+        for (int i = 1; i < arr9.length; i++) {
+            System.out.print(arr9[i]);
+        }
+        System.out.println();
+    }
 
 
 }
