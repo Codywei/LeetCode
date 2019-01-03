b1e175eb9b7fdb3514c08b1633a80dc14a2e07bf
CodyWei <33363378+Codywei@users.noreply.github.com>
2019-01-03 16:35:35 +0800

    字符串练习

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -2,4 +2,32 @@
  字符串练习
  **/
 public class StringPractice {
+    /**
+     1.两个字符串包含的字符是否完全相同
+     * */
+    public boolean isAnagram(String s, String t) {
+        int[] cnts = new int[26];
+        for (char c : s.toCharArray()) {
+            cnts[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            cnts[c - 'a']--;
+        }
+        for (int cnt : cnts) {
+            if (cnt != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        StringPractice sp=new StringPractice();
+
+
+        //第一题
+        String s1="net";
+        String s2="net";
+        System.out.println("两个字符串包含的字符是否完全相同: "+sp.isAnagram(s1,s2));
+    }
 }
