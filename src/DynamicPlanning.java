6115a34279b260f20f2b7c049f213777a2ad9e66
CodyWei <33363378+Codywei@users.noreply.github.com>
2019-01-10 10:53:37 +0800

    动态规划

--- src/DynamicPlanning.java
+++ src/DynamicPlanning.java
@@ -60,18 +60,260 @@ public class DynamicPlanning {
     }
 
 
-    public static void main(String[] args) {
+    /**
+     4.数组中等差递增子区间的个数
+
+     dp[i] 表示以 A[i] 为结尾的等差递增子区间的个数。
+
+     在 A[i] - A[i - 1] == A[i - 1] - A[i - 2] 的条件下，{A[i - 2], A[i - 1], A[i]} 是一个等差递增子区间。
+     如果 {A[i - 3], A[i - 2], A[i - 1]} 是一个等差递增子区间，那么 {A[i - 3], A[i - 2], A[i - 1], A[i]} 也是等差递增子区间，dp[i] = dp[i-1] + 1。
+     * */
+    public int numberOfArithmeticSlices(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int n = A.length;
+        int[] dp = new int[n];
+        for (int i = 2; i < n; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        int total = 0;
+        for (int cnt : dp) {
+            total += cnt;
+        }
+        return total;
+    }
+
+    /**
+     5.最长递增子序列
+
+     已知一个序列 {S1, S2,...,Sn}，取出若干数组成新的序列 {Si1, Si2,..., Sim}，其中 i1、i2 ... im 保持递增，即新序列中各个数仍然保持原数列中的先后顺序，称新序列为原序列的一个 子序列 。
+     如果在子序列中，当下标 ix > iy 时，Six > Siy，称子序列为原序列的一个 递增子序列 。
+
+     定义一个数组 dp 存储最长递增子序列的长度，dp[n] 表示以 Sn 结尾的序列的最长递增子序列长度。
+     对于一个递增子序列 {Si1, Si2,...,Sim}，如果 im < n 并且 Sim < Sn，此时 {Si1, Si2,..., Sim, Sn} 为一个递增子序列，递增子序列的长度增加 1。
+     满足上述条件的递增子序列中，长度最长的那个递增子序列就是要找的，在长度最长的递增子序列上加上 Sn 就构成了以 Sn 为结尾的最长递增子序列。
+     因此 dp[n] = max{ dp[i]+1 | Si < Sn && i < n} 。
+
+     因为在求 dp[n] 时可能无法找到一个满足条件的递增子序列，此时 {Sn} 就构成了递增子序列，需要对前面的求解方程做修改，令 dp[n] 最小为 1，即：
+
+                            dp[n] = max{ 1，dp[i]+1 | Si < Sn && i < n}
+
+     对于一个长度为 N 的序列，最长递增子序列并不一定会以 SN 为结尾，因此 dp[N] 不是序列的最长递增子序列的长度，需要遍历 dp 数组找出最大值才是所要的结果，max{ dp[i] | 1 <= i <= N} 即为所求。
+     * */
+
+    public int lengthOfLIS1(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            int max = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    max = Math.max(max, dp[j] + 1);
+                }
+            }
+            dp[i] = max;
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+
+    /**
+     以上解法的时间复杂度为 O(N2)，可以使用二分查找将时间复杂度降低为 O(NlogN)。
+
+     定义一个 tails 数组，其中 tails[i] 存储长度为 i + 1 的最长递增子序列的最后一个元素。对于一个元素 x，
+
+     如果它大于 tails 数组所有的值，那么把它添加到 tails 后面，表示最长递增子序列长度加 1；
+     如果 tails[i-1] < x <= tails[i]，那么更新 tails[i] = x。
+     例如对于数组 [4,3,6,5]，有：
+
+     tails      len      num
+     []         0        4
+     [4]        1        3
+     [3]        1        6
+     [3,6]      2        5
+     [3,5]      2        null
+     可以看出 tails 数组保持有序，因此在查找 Si 位于 tails 数组的位置时就可以使用二分查找。
+     * */
+
+    public int lengthOfLIS2(int[] nums) {
+        int n = nums.length;
+        int[] tails = new int[n];
+        int len = 0;
+        for (int num : nums) {
+            int index = binarySearch(tails, len, num);
+            tails[index] = num;
+            if (index == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    private int binarySearch(int[] tails, int len, int key) {
+        int l = 0, h = len;
+        while (l < h) {
+            int mid = l + (h - l) / 2;
+            if (tails[mid] == key) {
+                return mid;
+            } else if (tails[mid] > key) {
+                h = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+
+
+    /**
+     6.最长公共子序列
+
+
+     对于两个子序列 S1 和 S2，找出它们最长的公共子序列。
+
+     定义一个二维数组 dp 用来存储最长公共子序列的长度，其中 dp[i][j] 表示 S1 的前 i 个字符与 S2 的前 j 个字符最长公共子序列的长度。考虑 S1i 与 S2j 值是否相等，分为两种情况：
+
+     当 S1i==S2j 时，那么就能在 S1 的前 i-1 个字符与 S2 的前 j-1 个字符最长公共子序列的基础上再加上 S1i 这个值，最长公共子序列长度加 1，即 dp[i][j] = dp[i-1][j-1] + 1。
+     当 S1i != S2j 时，此时最长公共子序列为 S1 的前 i-1 个字符和 S2 的前 j 个字符最长公共子序列，或者 S1 的前 i 个字符和 S2 的前 j-1 个字符最长公共子序列，取它们的最大者，即 dp[i][j] = max{ dp[i-1][j], dp[i][j-1] }。
+     综上，最长公共子序列的状态转移方程为：
+
+                         dp[i][j] = dp[i-1][j-1] + 1,  S1i==S2j
+                         dp[i][j] = max{ dp[i-1][j], dp[i][j-1] }, S1i != S2j
+
+     对于长度为 N 的序列 S1 和长度为 M 的序列 S2，dp[N][M] 就是序列 S1 和序列 S2 的最长公共子序列长度。
+
+     与最长递增子序列相比，最长公共子序列有以下不同点：
+
+     针对的是两个序列，求它们的最长公共子序列。
+     在最长递增子序列中，dp[i] 表示以 Si 为结尾的最长递增子序列长度，子序列必须包含 Si ；
+     在最长公共子序列中，dp[i][j] 表示 S1 中前 i 个字符与 S2 中前 j 个字符的最长公共子序列长度，不一定包含 S1i 和 S2j。
+     在求最终解时，最长公共子序列中 dp[N][M] 就是最终解，而最长递增子序列中 dp[N] 不是最终解，因为以 SN 为结尾的最长递增子序列不一定是整个序列最长递增子序列，需要遍历一遍 dp 数组找到最大者.
+     * */
+    public int lengthOfLCS(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+
+
+    /**
+     7.0-1 背包
+     有一个容量为 N 的背包，要用这个背包装下物品的价值最大，这些物品有两个属性：体积 w 和价值 v。
+
+     定义一个二维数组 dp 存储最大价值，其中 dp[i][j] 表示前 i 件物品体积不超过 j 的情况下能达到的最大价值。设第 i 件物品体积为 w，价值为 v，根据第 i 件物品是否添加到背包中，可以分两种情况讨论：
+
+     第 i 件物品没添加到背包，总体积不超过 j 的前 i 件物品的最大价值就是总体积不超过 j 的前 i-1 件物品的最大价值，dp[i][j] = dp[i-1][j]。
+     第 i 件物品添加到背包中，dp[i][j] = dp[i-1][j-w] + v。
+     第 i 件物品可添加也可以不添加，取决于哪种情况下最大价值更大。因此，0-1 背包的状态转移方程为：
+
+                             dp[i][j] ={ dp[i-1][j],dp[i-1][j-w] + v}
+     * */
+
+    public int knapsack1(int W, int N, int[] weights, int[] values) {
+        int[][] dp = new int[N + 1][W + 1];
+        for (int i = 1; i <= N; i++) {
+            int w = weights[i - 1], v = values[i - 1];
+            for (int j = 1; j <= W; j++) {
+                if (j >= w) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[N][W];
+    }
+
+    /**
+     空间优化
+
+     在程序实现时可以对 0-1 背包做优化。
+     观察状态转移方程可以知道，前 i 件物品的状态仅与前 i-1 件物品的状态有关，因此可以将 dp 定义为一维数组，其中 dp[j] 既可以表示 dp[i-1][j] 也可以表示 dp[i][j]。
+     此时，
+                dp[j]=max(dp[j],dp[j-w]+v)
+
+     因为 dp[j-w] 表示 dp[i-1][j-w]，因此不能先求 dp[i][j-w]，以防将 dp[i-1][j-w] 覆盖(i的状态依赖于i-1)。也就是说要先计算 dp[i][j] 再计算 dp[i][j-w]，在程序实现时需要按倒序来循环求解。
+     * */
+
+    public int knapsack2(int W, int N, int[] weights, int[] values) {
+        int[] dp = new int[W + 1];
+        for (int i = 1; i <= N; i++) {
+            int w = weights[i - 1], v = values[i - 1];
+            for (int j = W; j >= 1; j--) {
+                if (j >= w) {
+                    dp[j] = Math.max(dp[j], dp[j - w] + v);
+                }
+            }
+        }
+        return dp[W];
+    }
+
+
+    /**
+     无法使用贪心算法的解释
+
+     0-1 背包问题无法使用贪心算法来求解，也就是说不能按照先添加性价比最高的物品来达到最优，这是因为这种方式可能造成背包空间的浪费，从而无法达到最优。
+     考虑下面的物品和一个容量为 5 的背包，如果先添加物品 0 再添加物品 1，那么只能存放的价值为 16，浪费了大小为 2 的空间。
+     最优的方式是存放物品 1 和物品 2，价值为 22.
+
+     id	w	v	v/w
+     0	1	6	6
+     1	2	10	5
+     2	3	12	4
+
+     * */
+
+
+
+
+
+
+     public static void main(String[] args) {
         DynamicPlanning dp=new DynamicPlanning();
 
-        //第一题
-        int arrays[]={2,3,4,0,2};
-        System.out.println("强盗最多能抢多少： "+dp.rob(arrays));
+         //第一题
+         int arrays[]={2,3,4,0,2};
+         System.out.println("强盗最多能抢多少： "+dp.rob(arrays));
 
-        //第二题
-        int[][]arrays2={{1,3,1},{1,5,1},{4,2,1}};
-        System.out.println("矩阵的最小路径和： "+dp.minPathSum(arrays2));
+         //第二题
+         int[][]arrays2={{1,3,1},{1,5,1},{4,2,1}};
+         System.out.println("矩阵的最小路径和： "+dp.minPathSum(arrays2));
 
-        //第三题
-        System.out.println("矩阵的总路径数是： "+dp.uniquePaths(5,4));
+         //第三题
+         System.out.println("矩阵的总路径数是： "+dp.uniquePaths(5,4));
+
+         //第四题
+         int[] arrays3={1,2,3,4};
+         System.out.println("数组中等差递增子区间的个数： "+dp.numberOfArithmeticSlices(arrays3));
+
+         //第五题
+         int[] arrays4={4,3,6,5};
+         System.out.println("最长递增子序列： "+dp.lengthOfLIS1(arrays4)+" "+dp.lengthOfLIS2(arrays4));
+
+
+         //第六题
+         int[] array5={1,3,4,5};
+         int[] array6={2,3,4,7};
+         System.out.println("最长公共子序列： "+dp.lengthOfLCS(array5,array6));
+
+
+         //第七题
+         int[] weight={1,2,3};
+         int[] value={6,10,12};
+         System.out.println("0-1b背包最大价值： "+dp.knapsack1(5,3,weight,value)+" "+dp.knapsack2(5,3,weight,value));
+
+
     }
 }
